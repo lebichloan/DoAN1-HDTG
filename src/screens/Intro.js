79986461720223b0a8c6052a6af9c88987d0d433
BichLoan <21521083@gm.uit.edu.vn>
2023-05-05 20:50:27 +0700

    process bar

--- src/screens/Intro.js
+++ src/screens/Intro.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {StyleSheet, SafeAreaView, View, Text, Image} from 'react-native';
+import * as Progress from 'react-native-progress';
 import CUSTOM_COLOR from '../constants/colors.js';
 import FONT_FAMILY from '../constants/fonts.js';
 
@@ -8,6 +9,7 @@ const {navigation} = props;
 const [status, setStatus] = useState('');
 return (
     <SafeAreaView style={styles.container}>
+        <Progress.Bar progress={0.3} width={200} />
     </SafeAreaView>
 );
 };
