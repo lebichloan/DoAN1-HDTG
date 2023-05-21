5c2f274d1a9209d0c6c85edc9dc15eae78505b29
21522683 <21522683@gm.uit.edu.vn>
2023-05-21 12:32:19 +0700

    Tinh fix App.js

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import MainNavigator from './src/navigation/navigation.js'
 
-function App(): JSX.Element {
+const App = () => {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#FFF'}}>
       <MainNavigator></MainNavigator>
     </SafeAreaView>
-  );}
+  )
+}
+
+export default App
 
 const styles = StyleSheet.create({
-});
-
-export default App;
+  
+})
