884b6013d89a168c70897b25500926722c0812ab
BichLoan <21521083@gm.uit.edu.vn>
2023-05-22 13:14:39 +0700

    ...

--- src/screens/Intro.js
+++ src/screens/Intro.js
@@ -1,16 +1,31 @@
 import React, {useState} from 'react';
-import {StyleSheet, SafeAreaView, View, Text, Image} from 'react-native';
-import CUSTOM_COLOR from '../constants/colors.js';
-import FONT_FAMILY from '../constants/fonts.js';
+import {StyleSheet, SafeAreaView, ImageBackground, Image} from 'react-native';
+import {IMG_Background} from '../assets/images/index.js';
 
-const Intro = props => {
-  const {navigation} = props;
-  const [status, setStatus] = useState('');
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
+const Intro = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* <ImageBackground
+        source={IMG_Background}
+        resizeMode="cover"
+        style={styles.image}
+      /> */}
+      <Image
+        source={IMG_Background}
+        style={[styles.container, {width: '100%', height: '100%'}]}
+      />
+    </SafeAreaView>
+  );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  // image: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  // },
 });
 export default Intro;
