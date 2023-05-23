321cb8d82ce1b2213536810d9056814ebacd18bc
BichLoan <21521083@gm.uit.edu.vn>
2023-05-23 13:36:32 +0700

    Mỹ nữ gất là mợt mỏi

--- src/components/Headers/Title.js
+++ src/components/Headers/Title.js
@@ -3,17 +3,17 @@ import {StyleSheet, Text} from 'react-native';
 import FONT_FAMILY from '../../constants/fonts';
 import CUSTOM_COLOR from '../../constants/colors';
 
-const HeaderTitlle = props => {
+const Title = props => {
   return <Text style={styles.titleView}>{props.title}</Text>;
 };
 const styles = StyleSheet.create({
   titleView: {
-    fontFamily: FONT_FAMILY.SemiBold,
+    fontFamily: FONT_FAMILY.Bold,
     fontSize: 30,
     color: CUSTOM_COLOR.Black,
     fontWeight: 'bold',
   },
 });
-export default HeaderTitlle;
+export default Title;
 
 // Header screeen 5,7,8,9
